ddf623c62fec6dbde8c206654b74822d9e8c9589
HN plays <92354190+HNwastaken@users.noreply.github.com>
2021-10-25 11:36:54 +0700

    Update minecraft.py

--- minecraft.py
+++ minecraft.py
@@ -1,6 +1,11 @@
-#this trust testing not work like minecraft yet but i will try to upgrade it but renimber to install ursina when run
+# it have a new update is void update (minecraft v1.0.1)
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
+import random
+
+def update():
+    if player.y <= -100:
+        player.position = (0, 50, 0)
 
 class Voxel(Button):
     def __init__(self, position = (0,0,0)):
@@ -10,7 +15,7 @@ class Voxel(Button):
             model = 'cube',
             origin_y = 0.5,
             texture = 'white_cube',
-            color = color.white,
+            color = color.color(0.2,0,random.uniform(0.9,1)),
             highlight_color = color.lime)
 
     def input(self, key):
@@ -26,9 +31,9 @@ window.fps_counter.enabled = False
 window.exit_button.visible = False
 
 for y in range(1):
-    for z in range(35):
-        for x in range(35):
-            voxel = Voxel(position=(x,y,z))
+    for z in range(36):
+        for x in range(36):
+            voxel = Voxel(position=(x-18,y,z-18))
 
 player = FirstPersonController()
 
